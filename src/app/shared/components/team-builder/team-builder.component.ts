719a1f3e5b4ae8284b5537300f261b3046650051
durielbaal <michaelogwu1997@gmail.com>
2025-01-12 19:13:26 +0100

    7.- Fixing some bugs. Adding Ivs and Evs system.

--- src/app/shared/components/team-builder/team-builder.component.ts
+++ src/app/shared/components/team-builder/team-builder.component.ts
@@ -12,10 +12,7 @@ interface Pokemon {
   ability: string;
   item: string;
   nature: string;
-  stats: Stats;
-}
-interface Stats {
-  attack: number,
+  attack: number;
   specialAttack: number,
   defense: number,
   specialDefense: number,
@@ -30,6 +27,7 @@ interface Stats {
 })
 
 export class TeamBuilderComponent {
+  
   menuVisible: boolean = false;
   selectedPokemonIndex: number = 0;
   items: string[] = ['None', 'Miracle Seed', 'Focus Sash'];
@@ -41,18 +39,8 @@ export class TeamBuilderComponent {
   selectedItem: string = this.pokemonCards[this.selectedPokemonIndex]?.item || 'None';
   selectedMove: string[] = this.pokemonCards[this.selectedPokemonIndex]?.moves || ['None','None','None','None'];
   selectedAbility: string = this.pokemonCards[this.selectedPokemonIndex]?.ability || 'None';
-  
-  get statsArray() {
-    const selectedPokemon = this.pokemonCards[this.selectedPokemonIndex];
-    return selectedPokemon ? [
-      { name: 'HP', value: selectedPokemon.stats.hp },
-      { name: 'Attack', value: selectedPokemon.stats.attack },
-      { name: 'Defense', value: selectedPokemon.stats.defense },
-      { name: 'Special Attack', value: selectedPokemon.stats.specialAttack },
-      { name: 'Special Defense', value: selectedPokemon.stats.specialDefense },
-      { name: 'Speed', value: selectedPokemon.stats.speed },
-    ] : [];
-  }
+  ivs: number[] = [0,0,0,0,0,0];
+  evs: number[] = [0,0,0,0,0,0];
   allPokemon: Pokemon[] =  [
     {
       pokedexNumber: 1,
@@ -63,14 +51,12 @@ export class TeamBuilderComponent {
       ability: 'Overgrow',
       item: 'None',
       nature: 'Docile',
-      stats: {
-        attack: 49,
-        specialAttack: 65,
-        defense: 49,
-        specialDefense: 65,
-        speed: 45,
-        hp: 45
-      }
+      attack: 49,
+      specialAttack: 65,
+      defense: 49,
+      specialDefense: 65,
+      speed: 45,
+      hp: 45,
     },
     {
       pokedexNumber: 2,
@@ -81,86 +67,12 @@ export class TeamBuilderComponent {
       ability: 'Overgrow',
       item: 'None',
       nature: 'Bold',
-      stats: {
-        attack: 62,
-        specialAttack: 80,
-        defense: 63,
-        specialDefense: 80,
-        speed: 60,
-        hp: 60
-      }
-    },
-    {
-      pokedexNumber: 3,
-      name: 'Venusaur',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
-      types: ['Grass', 'Poison'],
-      moves: ['Tackle', 'Growl', 'Solar Beam', 'Vine Whip'],
-      ability: 'Overgrow',
-      item: 'None',
-      nature: 'Calm',
-      stats: {
-        attack: 82,
-        specialAttack: 100,
-        defense: 83,
-        specialDefense: 100,
-        speed: 80,
-        hp: 80
-      }
-    },
-    {
-      pokedexNumber: 4,
-      name: 'Charmander',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-      types: ['Fire'],
-      moves: ['Scratch', 'Growl', 'Ember', 'Smokescreen'],
-      ability: 'Blaze',
-      item: 'None',
-      nature: 'Jolly',
-      stats: {
-        attack: 52,
-        specialAttack: 60,
-        defense: 43,
-        specialDefense: 50,
-        speed: 65,
-        hp: 39
-      }
-    },
-    {
-      pokedexNumber: 5,
-      name: 'Charmeleon',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
-      types: ['Fire'],
-      moves: ['Scratch', 'Growl', 'Flamethrower', 'Dragon Rage'],
-      ability: 'Blaze',
-      item: 'None',
-      nature: 'Adamant',
-      stats: {
-        attack: 64,
-        specialAttack: 80,
-        defense: 58,
-        specialDefense: 65,
-        speed: 80,
-        hp: 58
-      }
-    },
-    {
-      pokedexNumber: 6,
-      name: 'Charizard',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-      types: ['Fire', 'Flying'],
-      moves: ['Flamethrower', 'Fly', 'Slash', 'Fire Spin'],
-      ability: 'Blaze',
-      item: 'None',
-      nature: 'Modest',
-      stats: {
-        attack: 84,
-        specialAttack: 109,
-        defense: 78,
-        specialDefense: 85,
-        speed: 100,
-        hp: 78
-      }
+      attack:62,
+      specialAttack: 80,
+      defense: 63,
+      specialDefense: 80,
+      speed: 60,
+      hp: 60,
     },
     // El resto de los Pokémon seguirían con la misma estructura
   ];
@@ -218,10 +130,15 @@ export class TeamBuilderComponent {
     
   }
 
-  obtainStatsWithCertainLevel(stat: number,ivs:number,evs:number,level:number) : number{
+  obtainStatsWithCertainLevel(stat: number,ivs:number,evs:number,level:number,isPs: boolean) : number{
+    if(ivs > 31) ivs = 31;
+    if(ivs < 0) ivs = 0;
+    if(evs > 252) evs = 252
+    if(evs < 0) evs = 0
     evs = evs / 4;
     stat = 2 * stat;
-    let finalStat =(((stat + ivs + evs) * level) / 100)+ level + 10;
+    const finalPart = isPs ? level + 10: 5;
+    const finalStat= (((stat + ivs + evs) * level) / 100)+ finalPart;
     return finalStat;
   }
 
@@ -231,4 +148,5 @@ export class TeamBuilderComponent {
     this.selectedMove = this.pokemonCards[this.selectedPokemonIndex]?.moves || ['None','None','None','None'];
     this.selectedAbility = this.pokemonCards[this.selectedPokemonIndex]?.ability || 'None';
   }
+
 }
